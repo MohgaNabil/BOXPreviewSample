266724d76427c931b8322b4d821c90610197460d
Mohga Nabil <mohga.mkamel@gmail.com>
2017-05-07 22:52:50 +0200

    adding more urls for different video types

--- BOXPreviewPOC/ViewController.swift
+++ BOXPreviewPOC/ViewController.swift
@@ -17,8 +17,15 @@ class ViewController: UIViewController {
 		let clientSecret = "sAwYToBNyXjulHk1EiqDP4RhcaHrdM0a"
 		let redirectURI = "https://lighthouse.ibm.com"
 		BOXContentClient.setClientID(clientID, clientSecret: clientSecret, redirectURIString: redirectURI)
-		
-		previewBOXFile()
+		//webm video
+//		let url = URL(string: "https://ibm.box.com/s/79nsm88qqe8inq5543jkblved3gcd0kc")!
+		//asf video
+		//let url = URL(string: "https://ibm.box.com/s/897h5jeznihx1fgt99qqgetnvfgemna6")!
+		//m4v video
+		//let url = URL(string:"https://ibm.box.com/s/f379oawm507kx500o6x0wv85j3mvtsdd")!
+		//mov video
+		let url = URL(string:"https://ibm.box.com/s/08knplp2380lqdf3epwqaj4atemku6rc")!
+		previewBOXFile(url: url)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -27,10 +34,10 @@ class ViewController: UIViewController {
 	}
 	
 	
-	func previewBOXFile(){
+	func previewBOXFile(url:URL){
 		let client = BOXContentClient.default()!
 		if client.session.isAuthorized(){
-			self.getSharedLinkInfo(sharedLink: NSURL(string:"https://ibm.box.com/s/08knplp2380lqdf3epwqaj4atemku6rc")! as URL!){ (file, error) in
+			self.getSharedLinkInfo(sharedLink: url){ (file, error) in
 				if error == nil {
 					let previewViewController = BOXFilePreviewController(contentClient: BOXContentClient.default(), file: file!)!
 					previewViewController.view.translatesAutoresizingMaskIntoConstraints = false
@@ -43,7 +50,7 @@ class ViewController: UIViewController {
 		}else{
 			client.authenticate(completionBlock: { (user, error) in
 				if error == nil{
-					self.getSharedLinkInfo(sharedLink: NSURL(string:"https://ibm.box.com/s/08knplp2380lqdf3epwqaj4atemku6rc")! as URL!){ (file, error) in
+					self.getSharedLinkInfo(sharedLink: url){ (file, error) in
 						if error == nil {
 							let previewViewController = BOXFilePreviewController(contentClient: BOXContentClient.default(), file: file!)!
 							previewViewController.view.translatesAutoresizingMaskIntoConstraints = false
@@ -60,16 +67,27 @@ class ViewController: UIViewController {
 	}
 	
 	func getSharedLinkInfo(sharedLink:URL,completionHandler:@escaping (_ fileInfo:BOXFile?,_ error:NSError?)-> Void){
-		let client = BOXContentClient.default()!
-			client.sharedItemInfoRequest(withSharedLinkURL: sharedLink, password: "").perform { (item, error) in
-			if item != nil{
-				if item!.isFile{
-					completionHandler(item as? BOXFile,nil)
-				}
+//		let client = BOXContentClient.default()!
+//			client.sharedItemInfoRequest(withSharedLinkURL: sharedLink, password: "").perform { (item, error) in
+//			if item != nil{
+//				if item!.isFile{
+//					completionHandler(item as? BOXFile,nil)
+//				}
+//			}else{
+//				completionHandler(nil,error as NSError?)
+//			}
+//		}
+		let client = BOXPreviewClient(contentClient: BOXContentClient.default())!
+		let req = client.retrieveFile(forSharedLink: sharedLink) { (file, error) in
+			if file != nil{
+				completionHandler(file,nil)
+				
 			}else{
 				completionHandler(nil,error as NSError?)
 			}
+			
 		}
+
 		
 	}
 
